6c2673111230a0f1a5638ea05226e53d02330783
nitin10 <nitiniszod10@gmail.com>
2026-05-08 11:58:42 +0530

    fix: DocuVerse auto-fix for app/src/main/java/com/vacoder/womensafety/IntroFragment2.java

--- app/src/main/java/com/vacoder/womensafety/IntroFragment2.java
+++ app/src/main/java/com/vacoder/womensafety/IntroFragment2.java
@@ -1,3 +1,4 @@
+```java
 package com.vacoder.womensafety;
 
 import android.os.Bundle;
@@ -17,3 +18,4 @@ public class IntroFragment2 extends Fragment {
         return inflater.inflate(R.layout.intro_fragment_2, container, false);
     }
 }
+```
