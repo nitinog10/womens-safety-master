7200cc9bc6d3fe71cae32d40aba1c54a641a94ee
nitin10 <nitiniszod10@gmail.com>
2026-05-08 11:58:43 +0530

    fix: DocuVerse auto-fix for app/src/main/java/com/vacoder/womensafety/MainActivity.java

--- app/src/main/java/com/vacoder/womensafety/MainActivity.java
+++ app/src/main/java/com/vacoder/womensafety/MainActivity.java
@@ -1,3 +1,4 @@
+```java
 package com.vacoder.womensafety;
 
 import android.content.Intent;
@@ -25,8 +26,7 @@ public class MainActivity extends AppCompatActivity  {
     private static final String TAG = "MainActivity";
 
     private TextView tv_name;
-
-    SharedPreferences sharedPreferences;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,68 +38,69 @@ public class MainActivity extends AppCompatActivity  {
         tv_name = findViewById(R.id.tv_name);
         tv_name.setText(sharedPreferences.getString(USER_NAME, ""));
 
-        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        getSupportActionBar().setCustomView(R.layout.action_bar);
-
-        View view = getSupportActionBar().getCustomView();
-        ImageButton imageButton = view.findViewById(R.id.image_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EditDetailsActivity.class);
-                startActivityForResult(intent, 4);
-
-            }
-        });
-
-
+        setupActionBar();
         loadData();
-
     }
 
-    public void loadData() {
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!= null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setCustomView(R.layout.action_bar);
 
-        SharedPreferences sharedPreferences = getSharedPreferences(USER_DETAILS, MODE_PRIVATE);
+            View view = actionBar.getCustomView();
+            ImageButton imageButton = view.findViewById(R.id.image_button);
+            imageButton.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, EditDetailsActivity.class);
+                startActivityForResult(intent, 4);
+            });
+        }
+    }
+
+    private void loadData() {
         if (sharedPreferences.getString(USER_NAME, "").equals("")) {
             Intent intent = new Intent(MainActivity.this, IntroActivity.class);
             startActivityForResult(intent, 1);
         }
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            Intent intent = new Intent(MainActivity.this, GetDetailsActivity.class);
-            startActivityForResult(intent, 3);
-        } else if (requestCode == 4) {
-            if (data != null) {
-                tv_name.setText(data.getStringExtra(USER_NAME));
-            } else {
-                Toast.makeText(this, "Changing Data Cancelled", Toast.LENGTH_SHORT).show();
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 1:
+                    Intent intent = new Intent(MainActivity.this, GetDetailsActivity.class);
+                    startActivityForResult(intent, 3);
+                    break;
+                case 4:
+                    if (data!= null) {
+                        tv_name.setText(data.getStringExtra(USER_NAME));
+                    } else {
+                        Toast.makeText(this, "Changing Data Cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case 3:
+                    if (data!= null) {
+                        tv_name.setText(data.getStringExtra(USER_NAME));
+                    }
+                    break;
             }
-        } else if (requestCode == 3) {
-            tv_name.setText(data.getStringExtra(USER_NAME));
         }
     }
-
 
     public void alertMessage(View view) {
         GpsTracker gpsTracker = new GpsTracker(MainActivity.this);
         if (gpsTracker.canGetLocation()) {
-            double laitude = gpsTracker.getLatitude();
+            double latitude = gpsTracker.getLatitude();
             double longitude = gpsTracker.getLongitude();
             SmsManager smsManager = SmsManager.getDefault();
-            StringBuffer smsBody = new StringBuffer();
-            smsBody.append("http://maps.google.com?q=");
-            smsBody.append(String.valueOf(laitude));
-            smsBody.append(",");
-            smsBody.append(String.valueOf(longitude));
-            smsManager.sendTextMessage("9920193246", null, smsBody.toString(), null, null);
+            String smsBody = "http://maps.google.com?q=" + latitude + "," + longitude;
+            smsManager.sendTextMessage("9920193246", null, smsBody, null, null);
         } else {
             gpsTracker.showSettingsAlert();
         }
     }
 }
+```
